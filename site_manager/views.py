b452cc30851e470c5c42cb8aa0c564fca04722c1
kolxz2Aorus <kolxz2@mail.ru>
2024-05-16 15:12:24 +0300

    Фильтр для всех франшиз

--- site_manager/views.py
+++ site_manager/views.py
@@ -1,30 +1,43 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
-from personal_account.forms import AddFranchiseForm
+from personal_account.forms import AddFranchiseForm, FranchiseEditForm
 from personal_account.models import FranchisePhoto, Franchise
 
 
 @login_required
-def franchise_validation(request):
-    if not request.user.is_staff:
-        return redirect('login')
+def franchise_validation(request, franchise_id):
+    franchise = get_object_or_404(Franchise, pk=franchise_id)
+    # if franchise.user_id != request.user.id:
+    #     return redirect('unauthorized_access')
     if request.method == 'POST':
-        form = AddFranchiseForm(request.POST, request.FILES, request=request)
+        if 'delete_franchise' in request.POST:
+            franchise.delete()
+            return redirect('user_franchises')
+        form = AddFranchiseForm(request.POST, request.FILES, instance=franchise)
         franchise_photos = request.FILES.getlist('images')
-        if form.is_valid() and len(franchise_photos) > 0:
-            franchise = form.save()
+        if form.is_valid():
+            FranchisePhoto.objects.filter(franchise=franchise).delete()
+            form.save()
             for photo in franchise_photos:
                 FranchisePhoto.objects.create(franchise=franchise, franchise_photos=photo)
             return redirect('user_franchises')
     else:
-        form = AddFranchiseForm()
-    return render(request, 'franchise_validation.html', {'form': form})
+        form = FranchiseEditForm(instance=franchise)
+    return render(request, 'create_franchise.html', {'franchise': franchise, 'form': form})
 
 
 @login_required
 def all_site_franchises(request):
     if not request.user.is_staff:
         return redirect('login')
+
+    publish_filter = request.GET.get('publish_filter')
     franchises = Franchise.objects.all()
-    return render(request, 'all_site_franchises.html', {'franchises': franchises})
+
+    if publish_filter == 'published':
+        franchises = franchises.filter(allow_to_publish=True)
+    elif publish_filter == 'unpublished':
+        franchises = franchises.filter(allow_to_publish=False)
+
+    return render(request, 'all_site_franchises.html', {'franchises': franchises, 'publish_filter': publish_filter})
