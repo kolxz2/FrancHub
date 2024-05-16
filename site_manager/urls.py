b3ea77575960c5aada8515047daa5a7294e2eb0d
kolxz2Aorus <kolxz2@mail.ru>
2024-05-16 15:12:24 +0300

    Фильтр для всех франшиз

--- site_manager/urls.py
+++ site_manager/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-                  path('franchise_validation/', franchise_validation, name='franchise_validation'),
+                  path('franchise_validation/<int:franchise_id>/edit/', franchise_validation,
+                       name='franchise_validation'),
                   path('all_site_franchises/', all_site_franchises, name='all_site_franchises'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
